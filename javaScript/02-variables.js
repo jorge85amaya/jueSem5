37854c5a9f6f1ea1547e4c22dbe9569fca7460d7
Jorge Amaya <jorgebenildoamaya@gmail.com>
2022-11-20 18:52:04 -0300

    Variables javaScript

---
+++ javaScript/02-variables.js
@@ -0,0 +1,19 @@
+//VARIABLES
+// Una variable es un contenedor de informacion
+
+let pais = "España";
+let continente = "Europa";
+let antiguedad = "2021";
+let pais_y_continente = pais + " " + continente;
+
+console.log(pais, continente);
+alert(pais_y_continente);
+
+//Cambiamos los valores de las variables
+
+pais = "Argentina";
+continente = "Latinoamerica";
+pais_y_continente = `Pais:${pais}   Continente:${continente}`;
+
+console.log(pais, continente);
+alert(pais_y_continente);
